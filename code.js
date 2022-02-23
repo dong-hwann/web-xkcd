8e72fdb613740780205e8ee8fb5eec15d08cf357
dong-hwann <dong-hwan.kim@mines-paristech.fr>
2022-02-23 17:01:19 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
 const XKCD = "https://xkcd.now.sh/?comic="
 
 // write your code here
+function fetchIssue(num){
+    fetch("https://xkcd.now.sh/?comic = +num")
+}
+ow.addEventListener("load", () => {
+    document.getElementById("reset").addEventListener("click", () =>
+    )
+})
